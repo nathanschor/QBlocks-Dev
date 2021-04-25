2e57bb5be00c21fb932caf93a352ad5c677fe69f
JulianBeaulieu <julianbeaulieu@yahoo.com>
2021-04-25 02:06:13 -0700

    Updated Images

--- js/buttons.js
+++ js/buttons.js
@@ -4,32 +4,32 @@ let gateX = 4;
 let gateY = 4;
 
 function createPETE_Gate(){
-  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 2, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks/img/pete.png', 'peteGate', 'user');
+  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 2, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks-Dev/img/pete.png', 'peteGate', 'user');
   stage.add(layer);
 }
 
 function createSWAP_Gate(){
-  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 4, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks/img/swap.png', 'swapGate', 'user');
+  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 4, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks-Dev/img/swap.png', 'swapGate', 'user');
   stage.add(layer);
 }
 
 function createCSWAP_Gate(){
-  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 6, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks/img/cswap.png', 'cswapGate', 'user');
+  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 6, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks-Dev/img/cswap.png', 'cswapGate', 'user');
   stage.add(layer);
 }
 
 function createCCSWAP_Gate(){
-  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 8, 2, layer, stage, 'https://raw.githubusercontent.com/JulianBeaulieu/QBlocks/main/img/ccswap.png', 'ccswapGate', 'user');
+  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 8, 2, layer, stage, 'https://raw.githubusercontent.com/JulianBeaulieu/QBlocks-Dev/main/img/ccswap.png', 'ccswapGate', 'user');
   stage.add(layer);
 }
 
 function createCNOT_Gate(){
-  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 4, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks/img/cnot.png', 'cnotGate', 'user');
+  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 4, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks-Dev/img/cnot.png', 'cnotGate', 'user');
   stage.add(layer);
 }
 
 function createNOT_Gate(){
-  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 2, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks/img/not.png', 'notGate', 'user');
+  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 2, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks-Dev/img/not.png', 'notGate', 'user');
   stage.add(layer);
 }
 
@@ -44,12 +44,12 @@ function createWHITE_BALL(){
 }
 
 function createWBMist(){
-  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 4, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks/img/wb.png', 'wbMist', 'user');
+  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 4, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks-Dev/img/wb.png', 'wbMist', 'user');
   stage.add(layer);
 }
 
 function createWNegBMist(){
-  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 4, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks/img/wnegb.png', 'w-bMist', 'user');
+  newGate(blockSnapSize * gateX, blockSnapSize * gateY, 4, 2, layer, stage, 'https://julianBeaulieu.com/QBlocks-Dev/img/wnegb.png', 'w-bMist', 'user');
   stage.add(layer);
 }
 
