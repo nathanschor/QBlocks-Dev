ba1fec9929c59565f5dabdec123bcb72e72a3379
JulianBeaulieu <julianbeaulieu@yahoo.com>
2021-07-14 15:15:23 +0200

    Removed a rogue s

--- index.php
+++ index.php
@@ -52,6 +52,7 @@
                 return $file;
 
             $mtime = filemtime($_SERVER['DOCUMENT_ROOT'] . $file);
+            error_log(print_r($mtime, TRUE));
             return preg_replace('{\\.([^./]+)$}', ".$mtime.\$1", $file);
         }
     ?>
@@ -587,7 +588,7 @@
     <link rel="stylesheet" href="<?php echo auto_version('css/canvas.css'); ?>" type="text/css" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">s
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
